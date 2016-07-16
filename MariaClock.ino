b9c6ac732c5d6900265bb7f80753bec485578fa7
Karl Ostendorf <karl@ostendorf.com>
2016-07-16 11:07:55 +0200

    use ntp for time, mqtt for sensors

--- MariaClock.ino
+++ MariaClock.ino
@@ -1,21 +1,16 @@
 #define FW_NAME "mariaclock"
-#define FW_VERSION "2.0.12"
+#define FW_VERSION "2.1.2"
 #include <Homie.h>
 #include <TimeLib.h>   // https://github.com/PaulStoffregen/Time
 #include <Timezone.h>  // https://github.com/tauonteilchen/Timezone https://github.com/JChristensen/Timezone
 #include <Adafruit_LEDBackpack.h>
 #include <Adafruit_GFX.h>
+#include <WiFiUdp.h>
 
 /* Magic sequence for Autodetectable Binary Upload */
 const char *__FLAGGED_FW_NAME = "\xbf\x84\xe4\x13\x54" FW_NAME "\x93\x44\x6b\xa7\x75";
 const char *__FLAGGED_FW_VERSION = "\x6a\x3f\x3e\x0e\xe1" FW_VERSION "\xb0\x30\x48\xd4\x1a";
 /* End of magic sequence for Autodetectable Binary Upload */
-
-// Central European Time (Berlin, Madrid, Paris, Rome, Warsaw)
-TimeChangeRule CEST = {"CEST", Last, Sun, Mar, 2, 120};  // Central European Summer Time
-TimeChangeRule CET = {"CET", Last, Sun, Oct, 3, 60};    // Central European Standard Time
-Timezone timezone(CEST, CET);
-int timeLastMinute = -1; // last recorded minute
 
 const int LUX_PIN = 0;
 const int LUX_POLL_INTERVAL_MS = 100;   // 100 ms
@@ -24,11 +19,26 @@ unsigned long luxLastPoll = 0;
 unsigned long luxLastSent = 0;
 int luxLastReading = 0;
 
+// Central European Time (Berlin, Madrid, Paris, Rome, Warsaw)
+TimeChangeRule CEST = {"CEST", Last, Sun, Mar, 2, 120};  // Central European Summer Time
+TimeChangeRule CET = {"CET", Last, Sun, Oct, 3, 60};    // Central European Standard Time
+Timezone timezone(CEST, CET);
+
+WiFiUDP Udp;
+unsigned int localPort = 8888;  // local port to listen for UDP packets
+static const char ntpServerName[] = "raspberrypi";
+IPAddress ntpServerIP;
+const int NTP_POLL_INTERVAL_MS = 300000;  // 5 minutes
+const int NTP_PACKET_EXPIRED_MS = 1500;  // 1.5 seconds
+unsigned long ntpLastSent = 0;
+unsigned long ntpLastReceived = 0;
+
+int timeLastMinute = -1; // last recorded minute
+Adafruit_7segment matrix = Adafruit_7segment();
+
 // id, type, handler
 HomieNode nLux("lux", "lux");
 HomieNode nTime("time", "time");
-
-Adafruit_7segment matrix = Adafruit_7segment();
 
 void setup() {
   Homie.setBrand("kwo");
@@ -48,34 +58,37 @@ void loop() {
 
 void hSetup() {
   matrix.begin(0x70);
-  nTime.subscribe("value", hTime);
-}
-
-bool hTime(String value) {
-  if (isNumeric(value)) {
-    time_t t = value.toInt();
-    setTime(t);
-    if (Homie.setNodeProperty(nTime, "value", value, true)) {
-      Serial.print("time set ");
-      Serial.println(value);
-    } else {
-      Serial.println("set time value failed " + value);
-    }
-    return true;
-  }
-  Serial.println("set time failed " + value);
-  return false;
+  Udp.begin(localPort);
+  WiFi.hostByName(ntpServerName, ntpServerIP);
 }
 
 void hLoop() {
+
+  if (ntpLastSent == 0 && (timeStatus() == timeNotSet || (millis() - ntpLastReceived >= NTP_POLL_INTERVAL_MS))) {
+    ntpSendPacket(ntpServerIP);
+    ntpLastSent = millis();
+  }
+
+  if (ntpLastSent != 0) {
+    time_t t = ntpCheckPacket();
+    if (t != 0) {
+      ntpLastReceived = millis();
+      ntpLastSent = 0;
+      setTime(t);
+      Homie.setNodeProperty(nTime, "value", String(t), true);
+    } else if (millis() - ntpLastSent >= NTP_PACKET_EXPIRED_MS) {
+      ntpLastSent = 0;
+    }
+  }
 
   if (millis() - luxLastPoll >= LUX_POLL_INTERVAL_MS) {
 
     luxLastPoll = millis();
 
     int luxRaw = analogRead(LUX_PIN);
+    // typical readings do not often exceed 512, so limit range to 512 and then map
     int luxReading = map(constrain(luxRaw, 0, 511), 0, 511, 0, 15);
-    
+
     matrix.setBrightness(luxReading);
 
     if (millis() - luxLastSent >= LUX_SEND_INTERVAL_MS) {
@@ -144,13 +157,50 @@ void updateDisplay() {
 
 }
 
-bool isNumeric(String str) {
-  for (char i = 0; i < str.length(); i++) {
-    if ( !isDigit(str.charAt(i)) ) {
-      return false;
-    }
+/*-------- NTP code ----------*/
+
+const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
+byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
+
+time_t ntpCheckPacket() {
+
+  int size = Udp.parsePacket();
+  if (size >= NTP_PACKET_SIZE) {
+    Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
+    unsigned long secsSince1900;
+    // convert four bytes starting at location 40 to a long integer
+    secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
+    secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
+    secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
+    secsSince1900 |= (unsigned long)packetBuffer[43];
+    return secsSince1900 - 2208988800UL;
   }
-  return true;
+
+  return 0;
+
 }
 
+// send an NTP request to the time server at the given address
+void ntpSendPacket(IPAddress &address) {
+  while (Udp.parsePacket() > 0) ; // discard any previously received packets
+
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12] = 49;
+  packetBuffer[13] = 0x4E;
+  packetBuffer[14] = 49;
+  packetBuffer[15] = 52;
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  Udp.beginPacket(address, 123); //NTP requests are to port 123
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  Udp.endPacket();
+}
 
