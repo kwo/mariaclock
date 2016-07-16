cd273043c918dc8256f2334a12add32692b69158
Karl Ostendorf <karl@ostendorf.com>
2016-07-16 07:13:32 +0200

    initial commit

--- MariaClock.ino
+++ MariaClock.ino
@@ -1,91 +1,108 @@
-/*
- * MariaClock
- * v1.0 25.06.2016
- */
-
-#include <ESP8266WiFi.h>
-#include <WiFiUdp.h>
+#define FW_NAME "mariaclock"
+#define FW_VERSION "2.0.12"
+#include <Homie.h>
 #include <TimeLib.h>   // https://github.com/PaulStoffregen/Time
 #include <Timezone.h>  // https://github.com/tauonteilchen/Timezone https://github.com/JChristensen/Timezone
-#include "Adafruit_LEDBackpack.h"
-#include "Adafruit_GFX.h"
+#include <Adafruit_LEDBackpack.h>
+#include <Adafruit_GFX.h>
 
-const char ssid[] = "*****";  // your network SSID (name)
-const char pass[] = "*****";  // your network password
-
-static const char ntpServerName[] = "raspberrypi";
+/* Magic sequence for Autodetectable Binary Upload */
+const char *__FLAGGED_FW_NAME = "\xbf\x84\xe4\x13\x54" FW_NAME "\x93\x44\x6b\xa7\x75";
+const char *__FLAGGED_FW_VERSION = "\x6a\x3f\x3e\x0e\xe1" FW_VERSION "\xb0\x30\x48\xd4\x1a";
+/* End of magic sequence for Autodetectable Binary Upload */
 
 // Central European Time (Berlin, Madrid, Paris, Rome, Warsaw)
 TimeChangeRule CEST = {"CEST", Last, Sun, Mar, 2, 120};  // Central European Summer Time
-TimeChangeRule CET = {"CET ", Last, Sun, Oct, 3, 60};    // Central European Standard Time
+TimeChangeRule CET = {"CET", Last, Sun, Oct, 3, 60};    // Central European Standard Time
 Timezone timezone(CEST, CET);
+int timeLastMinute = -1; // last recorded minute
 
-WiFiUDP Udp;
-unsigned int localPort = 8888;  // local port to listen for UDP packets
+const int LUX_PIN = 0;
+const int LUX_POLL_INTERVAL_MS = 100;   // 100 ms
+const int LUX_SEND_INTERVAL_MS = 10000;  // 10 seconds
+unsigned long luxLastPoll = 0;
+unsigned long luxLastSent = 0;
+int luxLastReading = 0;
 
-int photocellPin = 0;
-int photocellReading;
-int ledBrightness = 0;
+// id, type, handler
+HomieNode nLux("lux", "lux");
+HomieNode nTime("time", "time");
 
 Adafruit_7segment matrix = Adafruit_7segment();
 
 void setup() {
-
-  Serial.begin(9600);
-  delay(250);
-
-  Serial.println("NTPClock");
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, pass);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-
-  Serial.print("IP: ");
-  Serial.println(WiFi.localIP());
-
-  Serial.println("Starting UDP");
-  Udp.begin(localPort);
-  Serial.print("Local port: ");
-  Serial.println(Udp.localPort());
-  Serial.println("waiting for sync");
-
-  setSyncProvider(getNtpTime);
-  setSyncInterval(300);
-
-  matrix.begin(0x70);
-
+  Homie.setBrand("kwo");
+  Homie.setFirmware(FW_NAME, FW_VERSION);
+  Homie.disableResetTrigger();
+  Homie.enableBuiltInLedIndicator(false);
+  Homie.registerNode(nLux);
+  Homie.registerNode(nTime);
+  Homie.setSetupFunction(hSetup);
+  Homie.setLoopFunction(hLoop);
+  Homie.setup();
 }
 
-int lastMinute = -1; // last recorded minute
-
 void loop() {
+  Homie.loop();
+}
 
-  photocellReading = analogRead(photocellPin); 
-  ledBrightness = map(photocellReading, 0, 1023, 0, 15);
-  matrix.setBrightness(ledBrightness);
-  Serial.print("brightness ");
-  Serial.print(photocellReading);     // the raw analog reading
-  Serial.print(" ");
-  Serial.println(ledBrightness);
-  
+void hSetup() {
+  matrix.begin(0x70);
+  nTime.subscribe("value", hTime);
+}
+
+bool hTime(String value) {
+  if (isNumeric(value)) {
+    time_t t = value.toInt();
+    setTime(t);
+    if (Homie.setNodeProperty(nTime, "value", value, true)) {
+      Serial.print("time set ");
+      Serial.println(value);
+    } else {
+      Serial.println("set time value failed " + value);
+    }
+    return true;
+  }
+  Serial.println("set time failed " + value);
+  return false;
+}
+
+void hLoop() {
+
+  if (millis() - luxLastPoll >= LUX_POLL_INTERVAL_MS) {
+
+    luxLastPoll = millis();
+
+    int luxRaw = analogRead(LUX_PIN);
+    int luxReading = map(constrain(luxRaw, 0, 511), 0, 511, 0, 15);
+    
+    matrix.setBrightness(luxReading);
+
+    if (millis() - luxLastSent >= LUX_SEND_INTERVAL_MS) {
+      if (luxReading != luxLastReading) {
+        luxLastReading = luxReading;
+        if (Homie.setNodeProperty(nLux, "value", String(luxLastReading), true)) {
+          luxLastSent = millis();
+        }
+        Homie.setNodeProperty(nLux, "raw", String(luxRaw), true);
+      }
+    }
+
+  }
+
   if (timeStatus() != timeNotSet) {
     int mm = minute();
-    if (mm != lastMinute) { //update the display only if time has changed
-      lastMinute = mm;
+    if (mm != timeLastMinute) { //update the display only if time has changed
+      timeLastMinute = mm;
       updateDisplay();
-      delay(100);
     }
   }
+
 }
 
 void updateDisplay() {
 
-  bool dots = true;          
+  bool dots = true;
   matrix.drawColon(dots);
 
   time_t local = timezone.toLocal(now());
@@ -93,96 +110,47 @@ void updateDisplay() {
   int mm = minute(local);
 
   if (hh < 10) {
-     matrix.writeDigitNum(0, 0, dots);
-     matrix.writeDigitNum(1, hh, dots);
+    matrix.writeDigitNum(0, 0, dots);
+    matrix.writeDigitNum(1, hh, dots);
   } else if (hh < 20) {
-     matrix.writeDigitNum(0, 1, dots);
-     matrix.writeDigitNum(1, hh - 10, dots);
+    matrix.writeDigitNum(0, 1, dots);
+    matrix.writeDigitNum(1, hh - 10, dots);
   } else {
-     matrix.writeDigitNum(0, 2, dots);
-     matrix.writeDigitNum(1, hh - 20, dots);
-  }    
+    matrix.writeDigitNum(0, 2, dots);
+    matrix.writeDigitNum(1, hh - 20, dots);
+  }
 
   if (mm < 10) {
-     matrix.writeDigitNum(3, 0, dots);
-     matrix.writeDigitNum(4, mm, dots);
+    matrix.writeDigitNum(3, 0, dots);
+    matrix.writeDigitNum(4, mm, dots);
   } else if (mm < 20) {
-     matrix.writeDigitNum(3, 1, dots);
-     matrix.writeDigitNum(4, mm - 10, dots);
+    matrix.writeDigitNum(3, 1, dots);
+    matrix.writeDigitNum(4, mm - 10, dots);
   } else if (mm < 30) {
-     matrix.writeDigitNum(3, 2, dots);
-     matrix.writeDigitNum(4, mm - 20, dots);
+    matrix.writeDigitNum(3, 2, dots);
+    matrix.writeDigitNum(4, mm - 20, dots);
   } else if (mm < 40) {
-     matrix.writeDigitNum(3, 3, dots);
-     matrix.writeDigitNum(4, mm - 30, dots);
+    matrix.writeDigitNum(3, 3, dots);
+    matrix.writeDigitNum(4, mm - 30, dots);
   } else if (mm < 50) {
-     matrix.writeDigitNum(3, 4, dots);
-     matrix.writeDigitNum(4, mm - 40, dots);
+    matrix.writeDigitNum(3, 4, dots);
+    matrix.writeDigitNum(4, mm - 40, dots);
   } else {
-     matrix.writeDigitNum(3, 5, dots);
-     matrix.writeDigitNum(4, mm - 50, dots);
+    matrix.writeDigitNum(3, 5, dots);
+    matrix.writeDigitNum(4, mm - 50, dots);
   }
 
   matrix.writeDisplay();
 
 }
 
-/*-------- NTP code ----------*/
-
-const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
-byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
-
-time_t getNtpTime() {
-
-  IPAddress ntpServerIP; // NTP server's ip address
-
-  while (Udp.parsePacket() > 0) ; // discard any previously received packets
-  Serial.println("Transmit NTP Request");
-  // get a random server from the pool
-  WiFi.hostByName(ntpServerName, ntpServerIP);
-  Serial.print(ntpServerName);
-  Serial.print(": ");
-  Serial.println(ntpServerIP);
-  sendNTPpacket(ntpServerIP);
-  uint32_t beginWait = millis();
-  while (millis() - beginWait < 1500) {
-    int size = Udp.parsePacket();
-    if (size >= NTP_PACKET_SIZE) {
-      Serial.println("Receive NTP Response");
-      Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
-      unsigned long secsSince1900;
-      // convert four bytes starting at location 40 to a long integer
-      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
-      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
-      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
-      secsSince1900 |= (unsigned long)packetBuffer[43];
-      return secsSince1900 - 2208988800UL;
+bool isNumeric(String str) {
+  for (char i = 0; i < str.length(); i++) {
+    if ( !isDigit(str.charAt(i)) ) {
+      return false;
     }
   }
-
-  Serial.println("No NTP Response :-(");
-  return 0; // return 0 if unable to get the time
-
+  return true;
 }
 
-// send an NTP request to the time server at the given address
-void sendNTPpacket(IPAddress &address) {
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12] = 49;
-  packetBuffer[13] = 0x4E;
-  packetBuffer[14] = 49;
-  packetBuffer[15] = 52;
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:
-  Udp.beginPacket(address, 123); //NTP requests are to port 123
-  Udp.write(packetBuffer, NTP_PACKET_SIZE);
-  Udp.endPacket();
-}
+
